d4d63962fe389d2398cd388f48ed7c34f7f7ba8e
hekar <hekar1987@gmail.com>
2022-08-11 02:10:12 +0300

    fix

--- src/components/AllLocation.js
+++ src/components/AllLocation.js
@@ -20,7 +20,7 @@ export default function Example() {
 
   return (
     <div className="bg-teal-100">
-      <div className="max-w-2xl mx-auto pt-16 px-4 sm:pt-24 sm:px-6 lg:max-w-7xl lg:px-8 text-center">
+      <div className="max-w-2xl mx-auto pt-16 px-4 sm:pt-24 sm:px-6 lg:max-w-7xl lg:px-8 text-center ">
         <h2 className="text-3xl font-bold tracking-tight text-gray-800 ">
           Find locations that interest you
         </h2>
@@ -28,17 +28,20 @@ export default function Example() {
           Collections of Locations in cities
         </h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8">
+        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8 ">
           {location.map((location) => (
-            <div key={location.lid} className="group relative">
-              <div className="w-full min-h-8 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+            <div
+              key={location.lid}
+              className="group relative bg-cyan-200 rounded-md border border-gray-400"
+            >
+              <div className="w-full min-h-8 bg-gray-200 aspect-w-1 aspect-h-1  rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                 <img
                   src={location.image}
                   alt={location.image}
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
               </div>
-              <div className="mt-2 flex justify-between">
+              <div className="mt-2 flex justify-between px-3 py-1">
                 <div>
                   <h3 className="text-xl font-semibold text-gray-900">
                     <a href={location.href}>
@@ -51,7 +54,7 @@ export default function Example() {
                   City: {location.locationname}
                 </p>
               </div>
-              <div className="mt-2 flex justify-between">
+              <div className="mt-2 flex justify-between px-3 pb-2">
                 <div>
                   <h3 className="text-xl font-semibold text-gray-900">
                     <a href={location.href}>
@@ -60,7 +63,7 @@ export default function Example() {
                       <ReactStars
                         count={location.rating}
                         size={24}
-                        edit = {false}
+                        edit={false}
                         value={location.rating}
                         isHalf={true}
                         emptyIcon={<i className="far fa-star"></i>}

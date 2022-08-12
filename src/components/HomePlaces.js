7d9bdbac72a198fa3d2f8e3584dd2eabe09df101
hekar <hekar1987@gmail.com>
2022-08-12 15:58:43 +0300

    add count to the city location

--- src/components/HomePlaces.js
+++ src/components/HomePlaces.js
@@ -1,107 +1,154 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
-import {
-  collection,
- 
-  onSnapshot,
-  orderBy,
-  query,
-  limit,
-} from "firebase/firestore";
-
-const Locations = [
-  {
-    id: 1,
-    name: "Duhok Mall",
-    href: "#",
-    imageSrc:
-      "http://www.marsmekanik.com/Upload/Galeri/Kucuk/6122016112023947.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    cityOfLocation: "Duhok",
-  },
-  {
-    id: 2,
-    name: "Erbil Citadel",
-    href: "#",
-    imageSrc:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/fb/e3/3a/photo1jpg.jpg?w=1200&h=-1&s=1",
-    imageAlt: "Front of men's Basic Tee in black.",
-    cityOfLocation: "Erbil",
-  },
-  {
-    id: 3,
-    name: "Ahmad Awa",
-    href: "#",
-    imageSrc:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/44/18/c1/this-beautiful-resort.jpg?w=1200&h=-1&s=1",
-    imageAlt: "Front of men's Basic Tee in black.",
-    cityOfLocation: "Sulaimaniya",
-  },
-  // More Locations...
-];
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { Link } from "react-router-dom";
 
 export default function Example() {
-  const [location, Setlocation] = useState([]);
+  const [dlocation, Setdlocation] = useState([]);
+  const [elocation, Setelocation] = useState([]);
+  const [slocation, Setslocation] = useState([]);
 
   useEffect(() => {
     // const locationCollection = collection(db, "Location");
-    // const getLocations = async () => {
+    // const getAllLocations = async () => {
     //   const data = await getDocs(locationCollection);
     //   Setlocation(data.docs.map((doc) => ({ ...doc.data(), id: doc.lid })));
 
     // };
     onSnapshot(
-      query(collection(db, "Location"), orderBy("rating"), limit(3)),
+      query(collection(db, "Location"), where("locationname", "==", "Duhok")),
       (snapshot) => {
-        Setlocation(
+        Setdlocation(
           snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.lid }))
         );
       }
     );
 
-    // getLocations();
+    onSnapshot(
+      query(collection(db, "Location"), where("locationname", "==", "Erbil")),
+      (snapshot) => {
+        Setelocation(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.lid }))
+        );
+      }
+    );
+
+    onSnapshot(
+      query(
+        collection(db, "Location"),
+        where("locationname", "==", "Sulaimaniya")
+      ),
+      (snapshot) => {
+        Setslocation(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.lid }))
+        );
+      }
+    );
+
+    // getAllLocations();
   }, []);
 
-  console.log(location);
+  console.log(dlocation);
+  const dlingth = Object.keys(dlocation).length;
+  const elingth = Object.keys(elocation).length;
+  const slingth = Object.keys(slocation).length;
+
+  // console.log(dlingth);
 
   return (
     <div className="bg-teal-100">
       <div className="max-w-2xl mx-auto pt-16 px-4 sm:pt-24 sm:px-6 lg:max-w-7xl lg:px-8 text-center">
         <h2 className="text-3xl font-bold tracking-tight text-gray-800 ">
-          Find locations that interest you
+          Find Alllocations that interest you
         </h2>
         <h2 className="text-xl tracking-tight text-gray-500  mt-4">
-          Collections of Locations in cities
+          Collections of AllLocations in cities
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8">
-          {Locations.map((location) => (
-            <div
-              key={location.id}
-              className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
-            >
-              <div className="w-full  min-h-8 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                <img
-                  src={location.imageSrc}
-                  alt={location.imageAlt}
-                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                />
-              </div>
-              <div className="mt-2 flex justify-between p-3">
-                <div>
-                  <h3 className="text-xl font-semibold text-gray-900 ">
-                    <a href={location.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {location.name}
-                    </a>
-                  </h3>
-                </div>
-                <p className="text-sm font-medium mt-1 text-gray-900">
-                  {location.numberOfLocation} Locations
-                </p>
-              </div>
+          {/* Duhok location */}
+          <div
+            key={1}
+            className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
+          >
+            <div className="w-full  min-h-8 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src="http://www.marsmekanik.com/Upload/Galeri/Kucuk/6122016112023947.jpg"
+                alt="Duhok"
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
             </div>
-          ))}
+            <div className="mt-2 flex justify-between p-3">
+              <div>
+                <h3 className="text-xl font-semibold text-gray-900 ">
+                  <Link to="/">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Duhok
+                  </Link>
+                </h3>
+              </div>
+              <p className="text-sm font-medium mt-1 text-gray-900">
+                {dlingth} Locations
+              </p>
+            </div>
+          </div>
+          {/* location */}
+
+          {/* Hawler location */}
+          <div
+            key={2}
+            className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
+          >
+            <div className="w-full  min-h-8 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/fb/e3/3a/photo1jpg.jpg?w=1200&h=-1&s=1"
+                alt="Hawler"
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="mt-2 flex justify-between p-3">
+              <div>
+                <h3 className="text-xl font-semibold text-gray-900 ">
+                  <Link to="/">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Erbil
+                  </Link>
+                </h3>
+              </div>
+              <p className="text-sm font-medium mt-1 text-gray-900">
+                {elingth} Locations
+              </p>
+            </div>
+          </div>
+          {/* Hawler location */}
+
+          {/* Sulaimany location */}
+          <div
+            key={1}
+            className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
+          >
+            <div className="w-full  min-h-8 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+              <img
+                src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/44/18/c1/this-beautiful-resort.jpg?w=1200&h=-1&s=1"
+                alt="Duhok"
+                className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+              />
+            </div>
+            <div className="mt-2 flex justify-between p-3">
+              <div>
+                <h3 className="text-xl font-semibold text-gray-900 ">
+                  <Link to="/">
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    Sulaimaniya
+                  </Link>
+                </h3>
+              </div>
+              <p className="text-sm font-medium mt-1 text-gray-900">
+                {slingth} Locations
+              </p>
+            </div>
+          </div>
+          {/* location */}
         </div>
       </div>
     </div>

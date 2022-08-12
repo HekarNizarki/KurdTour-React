69ec74103fa6049590f91b108200674e2cca3fb4
hekar <hekar1987@gmail.com>
2022-08-12 15:00:33 +0300

    get best location to the component

--- src/components/BestLocation.js
+++ src/components/BestLocation.js
@@ -1,35 +1,37 @@
-const Locations = [
-  {
-    id: 1,
-    name: "Duhok Mall",
-    href: "#",
-    imageSrc:
-      "http://www.marsmekanik.com/Upload/Galeri/Kucuk/6122016112023947.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    cityOfLocation: "Duhok",
-  },
-  {
-    id: 2,
-    name: "Erbil Citadel",
-    href: "#",
-    imageSrc:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/fb/e3/3a/photo1jpg.jpg?w=1200&h=-1&s=1",
-    imageAlt: "Front of men's Basic Tee in black.",
-    cityOfLocation: "Erbil",
-  },
-  {
-    id: 3,
-    name: "Ahmad Awa",
-    href: "#",
-    imageSrc:
-      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/44/18/c1/this-beautiful-resort.jpg?w=1200&h=-1&s=1",
-    imageAlt: "Front of men's Basic Tee in black.",
-    cityOfLocation: "Sulaimaniya",
-  },
-  // More Locations...
-];
+import { useState, useEffect } from "react";
+import { db } from "../firebase";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  limit,
+} from "firebase/firestore";
+import ReactStars from "react-rating-stars-component";
 
 export default function BestLocation() {
+  const [location, Setlocation] = useState([]);
+
+  useEffect(() => {
+    // const locationCollection = collection(db, "Location");
+    // const getLocations = async () => {
+    //   const data = await getDocs(locationCollection);
+    //   Setlocation(data.docs.map((doc) => ({ ...doc.data(), id: doc.lid })));
+
+    // };
+    onSnapshot(
+      query(collection(db, "Location"), orderBy("rating", "desc"), limit(3)),
+      (snapshot) => {
+        Setlocation(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.lid }))
+        );
+      }
+    );
+
+    // getLocations();
+  }, []);
+
+  console.log(location);
   return (
     <div className="bg-teal-100">
       <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8 text-center">
@@ -41,12 +43,15 @@ export default function BestLocation() {
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8">
-          {Locations.map((location) => (
-            <div key={location.id} className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md">
+          {location.map((location) => (
+            <div
+              key={location.id}
+              className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
+            >
               <div className="w-full min-h-8 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                 <img
-                  src={location.imageSrc}
-                  alt={location.imageAlt}
+                  src={location.image}
+                  alt={location.image}
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                 />
               </div>
@@ -55,12 +60,36 @@ export default function BestLocation() {
                   <h3 className="text-xl font-semibold text-gray-900">
                     <a href={location.href}>
                       <span aria-hidden="true" className="absolute inset-0" />
-                      {location.name}
+                      {location.title}
                     </a>
                   </h3>
                 </div>
                 <p className="text-sm font-medium mt-1 text-gray-900">
-                  City: {location.cityOfLocation}
+                  City: {location.locationname}
+                </p>
+              </div>
+              <div className="mt-2 flex justify-between px-3 pb-2">
+                <div>
+                  <h3 className="text-xl font-semibold text-gray-900">
+                    <a href={location.href}>
+                      <span aria-hidden="true" className="absolute inset-0" />
+
+                      <ReactStars
+                        count={location.rating}
+                        size={24}
+                        edit={false}
+                        value={location.rating}
+                        isHalf={true}
+                        emptyIcon={<i className="far fa-star"></i>}
+                        halfIcon={<i className="fa fa-star-half-alt"></i>}
+                        fullIcon={<i className="fa fa-star"></i>}
+                        activeColor="#ffd700"
+                      />
+                    </a>
+                  </h3>
+                </div>
+                <p className="text-sm font-medium mt-1 text-gray-900">
+                  Opening hours: {location.openhourse}
                 </p>
               </div>
             </div>
--- src/components/HomePlaces.js
+++ src/components/HomePlaces.js
@@ -1,36 +1,42 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+ 
+  onSnapshot,
+  orderBy,
+  query,
+  limit,
+} from "firebase/firestore";
 
 const Locations = [
   {
     id: 1,
-    name: "Duhok",
+    name: "Duhok Mall",
     href: "#",
     imageSrc:
-      "https://scontent.febl9-1.fna.fbcdn.net/v/t1.6435-9/89894444_140375824134976_9037075004302295040_n.jpg?stp=dst-jpg_p720x720&_nc_cat=110&ccb=1-7&_nc_sid=a26aad&_nc_eui2=AeEljvwCuL8wACVsZ9YRtWDX2LPnGLcCrE3Ys-cYtwKsTWTytkclcSbaieqWH__AZJft0IJpv1QL0e-EX7P4kf1f&_nc_ohc=2G4MwM1j4-sAX_X0SSV&_nc_ht=scontent.febl9-1.fna&oh=00_AT_qfUE4kvyT7N3f810KWpp6Ku5tNuoP_5MGr1YYq7bT0g&oe=6316A5CA",
+      "http://www.marsmekanik.com/Upload/Galeri/Kucuk/6122016112023947.jpg",
     imageAlt: "Front of men's Basic Tee in black.",
-    numberOfLocation: "3",
+    cityOfLocation: "Duhok",
   },
   {
     id: 2,
-    name: "Hewler",
+    name: "Erbil Citadel",
     href: "#",
     imageSrc:
-      "https://esta.krd/wp-content/uploads/2021/09/20640309329_24ecf4876a_b.jpg",
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/fb/e3/3a/photo1jpg.jpg?w=1200&h=-1&s=1",
     imageAlt: "Front of men's Basic Tee in black.",
-    numberOfLocation: "4",
+    cityOfLocation: "Erbil",
   },
   {
     id: 3,
-    name: "Sulaimani",
+    name: "Ahmad Awa",
     href: "#",
     imageSrc:
-      "https://www.rudaw.net/ContentFilesArchive/509856Image1.jpg?version=3620554",
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/44/18/c1/this-beautiful-resort.jpg?w=1200&h=-1&s=1",
     imageAlt: "Front of men's Basic Tee in black.",
-    numberOfLocation: "6",
+    cityOfLocation: "Sulaimaniya",
   },
-
   // More Locations...
 ];
 
@@ -38,13 +44,22 @@ export default function Example() {
   const [location, Setlocation] = useState([]);
 
   useEffect(() => {
-    const locationCollection = collection(db, "Location");
-    const getLocations = async () => {
-      const data = await getDocs(locationCollection);
-      Setlocation(data.docs.map((doc) => ({ ...doc.data(), id: doc.lid })));
-    };
+    // const locationCollection = collection(db, "Location");
+    // const getLocations = async () => {
+    //   const data = await getDocs(locationCollection);
+    //   Setlocation(data.docs.map((doc) => ({ ...doc.data(), id: doc.lid })));
 
-    getLocations();
+    // };
+    onSnapshot(
+      query(collection(db, "Location"), orderBy("rating"), limit(3)),
+      (snapshot) => {
+        Setlocation(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.lid }))
+        );
+      }
+    );
+
+    // getLocations();
   }, []);
 
   console.log(location);
@@ -61,7 +76,10 @@ export default function Example() {
 
         <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8">
           {Locations.map((location) => (
-            <div key={location.id} className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md">
+            <div
+              key={location.id}
+              className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
+            >
               <div className="w-full  min-h-8 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                 <img
                   src={location.imageSrc}

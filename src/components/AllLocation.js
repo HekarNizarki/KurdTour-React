7318a8c9fbcbaea0ce606af3fe2c6be3f4e28735
hekar <hekar1987@gmail.com>
2022-08-16 23:02:49 +0300

    fix

--- src/components/AllLocation.js
+++ src/components/AllLocation.js
@@ -4,7 +4,6 @@ import { collection, getDocs } from "firebase/firestore";
 import ReactStars from "react-rating-stars-component";
 import ReactLoading from "react-loading";
 
-
 export default function Example() {
   const [location, Setlocation] = useState([]);
 
@@ -25,82 +24,89 @@ export default function Example() {
       <div className="flex flex-row justify-center items-center ">
         <ReactLoading
           type="spinningBubbles"
-          color="#FF00FF"
-          height={200}
-          width={200}
+          color="#FFFFFF"
+          height={400}
+          width={400}
         />
       </div>
     );
   } else {
-  return (
-    <div className="bg-teal-100 pb-6">
-      <div className="max-w-2xl mx-auto pt-16 px-4 sm:pt-24 sm:px-6 lg:max-w-7xl lg:px-8 text-center ">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-800 ">
-          Find locations that interest you
-        </h2>
-        <h2 className="text-xl tracking-tight text-gray-500  mt-4">
-          Collections of Locations in cities
-        </h2>
+    return (
+      <div className="bg-teal-100 pb-6">
+        <div className="max-w-2xl mx-auto pt-16 px-4 sm:pt-24 sm:px-6 lg:max-w-7xl lg:px-8 text-center ">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-800 ">
+            Find locations that interest you
+          </h2>
+          <h2 className="text-xl tracking-tight text-gray-500  mt-4">
+            Collections of Locations in cities
+          </h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8 ">
-          {location.map((location) => (
-            <div
-              key={location.lid}
-              className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
-            >
-              <div className="w-full min-h-8 bg-gray-200 aspect-w-1 aspect-h-1  rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                <img
-                  src={location.image < 0 ? 
-                    (<ReactLoading 
-                  type="spinningBubbles"
-                  color="#FF00FF"
-                  height={200}
-                  width={200}/>) : location.image  }
-                  alt={location.image}
-                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                />
-              </div>
-              <div className="mt-2 flex justify-between px-3 py-1">
-                <div>
-                  <h3 className="text-xl font-semibold text-gray-900">
-                    <a href={location.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {location.title}
-                    </a>
-                  </h3>
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8 ">
+            {location.map((location) => (
+              <div
+                key={location.lid}
+                className="group relative bg-cyan-200 rounded-md border border-gray-400 shadow-md"
+              >
+                <div className="w-full min-h-8 bg-gray-200 aspect-w-1 aspect-h-1  rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                  <img
+                    src={
+                      location.image < 0 ? (
+                        <ReactLoading
+                          type="spinningBubbles"
+                          color="#FFFFFF"
+                          height={200}
+                          width={200}
+                        />
+                      ) : (
+                        location.image
+                      )
+                    }
+                    alt={location.image}
+                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                  />
                 </div>
-                <p className="text-sm font-medium mt-1 text-gray-900">
-                  City: {location.locationname}
-                </p>
-              </div>
-              <div className="mt-2 flex justify-between px-3 pb-2">
-                <div>
-                  <h3 className="text-xl font-semibold text-gray-900">
-                    <a href={location.href}>
-                      <span aria-hidden="true" className="absolute inset-0" />
+                <div className="mt-2 flex justify-between px-3 py-1">
+                  <div>
+                    <h3 className="text-xl font-semibold text-gray-900">
+                      <a href={location.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {location.title}
+                      </a>
+                    </h3>
+                  </div>
+                  <p className="text-sm font-medium mt-1 text-gray-900">
+                    City: {location.locationname}
+                  </p>
+                </div>
+                <div className="mt-2 flex justify-between px-3 pb-2">
+                  <div>
+                    <h3 className="text-xl font-semibold text-gray-900">
+                      <a href={location.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
 
-                      <ReactStars
-                        count={location.rating}
-                        size={24}
-                        edit={false}
-                        value={location.rating}
-                        isHalf={true}
-                        emptyIcon={<i className="far fa-star"></i>}
-                        halfIcon={<i className="fa fa-star-half-alt"></i>}
-                        fullIcon={<i className="fa fa-star"></i>}
-                        activeColor="#ffd700"
-                      />
-                    </a>
-                  </h3>
+                        <ReactStars
+                          count={location.rating}
+                          size={24}
+                          edit={false}
+                          value={location.rating}
+                          isHalf={true}
+                          emptyIcon={<i className="far fa-star"></i>}
+                          halfIcon={<i className="fa fa-star-half-alt"></i>}
+                          fullIcon={<i className="fa fa-star"></i>}
+                          activeColor="#ffd700"
+                        />
+                      </a>
+                    </h3>
+                  </div>
+                  <p className="text-xs font-medium mt-1 text-gray-900 sm:text-sm md:text-sm">
+                    Opening hours: {location.openhourse}
+                  </p>
                 </div>
-                <p className="text-xs font-medium mt-1 text-gray-900 sm:text-sm md:text-sm">
-                  Opening hours: {location.openhourse}
-                </p>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}}
+    );
+  }
+}
